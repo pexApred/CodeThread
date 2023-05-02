45b4d3ea11f8b362526d5a6b515ef665e5f37c28
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-01 20:47:34 -0400

    set up homepage.handlebars to test and homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    try {
+      res.render('homepage',);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
