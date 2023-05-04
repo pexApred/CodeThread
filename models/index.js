4c660b0cc0ba4fdb0c844019a4b9a65eb60fc4c3
pexApred <120075705+pexApred@users.noreply.github.com>
2023-05-04 19:01:26 -0400

    cleaned up models/index.js & models

--- models/index.js
+++ models/index.js
@@ -1,16 +1,15 @@
 // TODO: Define & lay out data for database (use sequelize and ORM)
 const User = require('./User');
 const Shirt = require('./Shirt');
-const Cohort = require('./Cohort');
 const ShirtOrder = require('./ShirtOrder');
 
-// A ShirtOrder belongs to one User
-ShirtOrder.belongsTo(User, {
+// User can have many ShirtOrders
+User.hasOne(ShirtOrder, {
     foreignKey: 'user_id',
 })
 
-// User can have many ShirtOrders
-User.hasMany(ShirtOrder, {
+// A ShirtOrder belongs to one User
+ShirtOrder.belongsTo(User, {
     foreignKey: 'user_id',
 })
 
@@ -19,32 +18,9 @@ Shirt.belongsTo(ShirtOrder, {
     foreignKey: 'shirtOrder_id',
 })
 
-// ShirtOrder can have many Shirts
-// When we delete a ShirtOrder, make sure to delete the associated Shirts
-ShirtOrder.hasMany(Shirt, {
-    foreignKey: 'shirtOrder_id',
-    onDelete: 'CASCADE',
-})
-
-// A Shirt belongs to one User
-Shirt.belongsTo(User, {
-    foreignKey: 'user_id',
-})
-
-// A User can have many Shirts
-// User.hasMany(Shirt, {
-//     foreignKey: 'user_id',
-// })
-
-// A Shirt belongs to one Cohort
-Shirt.belongsTo(Cohort, {
-    foreignKey: 'cohort_id',
-})
-
-// A Cohort can have many Shirts
-Cohort.hasMany(Shirt, {
-    foreignKey: 'cohort_id',
+Shirt.hasOne(ShirtOrder, {
+    foreignKey: 'shirt_id',
 })
 
 // TODO: Export models as an object
-module.exports = { User, Shirt, Cohort, ShirtOrder };
+module.exports = { User, Shirt, ShirtOrder };
