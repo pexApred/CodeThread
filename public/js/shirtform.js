814c701b4699066b60f1a87fc548036de4f6c037
mackenzie-wentworth <mwentworth28@gmail.com>
2023-05-07 19:48:48 -0400

    added idea of of 'cartButtonHandler' to shirtform.js; still needs some adjusting though!

--- public/js/shirtform.js
+++ public/js/shirtform.js
@@ -12,12 +12,14 @@ const shirtFormHandler = async (event) => {
     console.log(size, color)
 
     if (color && size && city && address && state && zip) {
+
         const response = await fetch('/api/shirtOrder', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({color, size, city, address, state, zip, apt}),
+            console
 
         });
         if (response.ok) {
@@ -29,4 +31,24 @@ const shirtFormHandler = async (event) => {
     }
 };
 
+// Trying to mimic 'delButtonHandler' from Mini Project in Module 14 (looking at 'profile.js' and 'profile.handlebars' from Mini Project) ---> Not sure if this will fix the shirtOrder route once user clicks "Add to Cart"???
+const cartButtonHandler = async (event) => {
+    if (event.target.hasAttribute('orderData-id')) {
+      const id = event.target.getAttribute('orderData-id');
+  
+      const response = await fetch(`/api/shirtOrder/${id}`, {
+        method: 'POST',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/shirtOrder');
+      } else {
+        alert('Failed to post shirt order!');
+      }
+    }
+  };
+
 document.querySelector('.new-shirt-form').addEventListener('submit', shirtFormHandler);
+
+document
+  .querySelector('.cart-order').addEventListener('click', cartButtonHandler);
