5c2e544abff9fecbe3d7051bfce742844982cec2
pexApred <120075705+pexApred@users.noreply.github.com>
2023-05-08 12:05:39 -0400

    added concatination helper for handlebars. fixed login/logout function. And fixed UI of shirt order.

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,3 +6,15 @@ const checked = function(value, test) {
 module.exports = {
     checked
 };
+
+const Handlebars = require('handlebars');
+
+Handlebars.registerHelper('concat', function() {
+  let concatenatedString = '';
+
+  for (let i = 0; i < arguments.length - 1; i++) {
+    concatenatedString += arguments[i];
+  }
+
+  return concatenatedString;
+});
